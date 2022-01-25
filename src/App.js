73d4d69f734b77d93a8b35dba188f121b8c4334f
AliPooladmast <ali.pooladmast.gh@gmail.com>
2022-01-25 16:14:55 +0330

    create navigation component (first commit)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav class="navigation">
+      <p class="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
